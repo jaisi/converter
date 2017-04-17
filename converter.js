98587587dd5469abfe295e4b4a70440fc07f4f21
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-04-17 18:10:59 -0500

    colors

--- converter.js
+++ converter.js
@@ -1,27 +1,63 @@
-var button=document.getElementById("converter");
+var button1=document.getElementById("converter");
 
-button.addEventListener("click", determineConverter);
+button1.addEventListener("click", determineConverter);
+
+var button2=document.getElementById("clear");
+button2.addEventListener("click", resetForm);
+
+x=document.getElementById("temp");
+
+
+x.addEventListener('keyup', event);
+
+ 
+ function event(q){
+
+  //check to see if the enter key was pressed
+  if (q.which === 13) {
+    determineConverter();
+  }
+ }
 
 
 function toCelsius (T) 
 {
 	console.log("in celcius function",T);
 
-	var C = (T - 32)/1.8;
-	document.getElementById("demo").innerHTML = "The temperature is " + C;
+	var C = Math.round((T - 32)/1.8,2);
+   if ( C > 32)
+  {
+     document.getElementById("demo").innerHTML = "<font color=red>The temperature is " + C + " degrees celcius</font>";
+  } 
+  else if ( C < 0)
+  {
+      document.getElementById("demo").innerHTML = "<font color=blue>The temperature is " + C + " degrees celcius</font>";
+  }
+  else
+  {
+    document.getElementById("demo").innerHTML = "<font color=green>The temperature is " + C + " degrees celcius</font>";
+  }
+	
 }
 
 function toFahrenheit (T)
  {
 	console.log("in fah function",T);
 
-	var F = T*1.8 + 32;
-	document.getElementById("demo").innerHTML = "The temperature is " + F;
+	var F = Math.round(T*1.8 + 32,2);
+  if ( F > 90)
+  {
+	   document.getElementById("demo").innerHTML = "<font color=red>The temperature is " + F + " degrees fahrenheit</font>";
+  } else if (F < 32)
+  {
+    document.getElementById("demo").innerHTML = "<font color=blue>The temperature is " + F +" degrees fahrenheit</font>";
+  }
+  else
+  {
+      document.getElementById("demo").innerHTML = "<font color=green>The temperature is " + F + " degrees fahrenheit</font>";
+  }
 
 }
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
@@ -35,11 +71,21 @@ function determineConverter ()
   	
   		toFahrenheit(temperature);
 
-  	} else
+  	} else if (document.getElementById("cel").checked)
   	{
-  	console.log("celcius checked");
-  	toCelsius(temperature);
-  }
+    	console.log("celcius checked");
+    	toCelsius(temperature);
+    } else
+    {
+      alert("You need to select one of the radio buttons");
+      
+    }
+}
+
+function resetForm()
+{
+  document.getElementById("temp").value = "";
+  document.getElementById("demo").innerHTML="";
 }
 
 
